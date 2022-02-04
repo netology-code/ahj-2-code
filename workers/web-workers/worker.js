8173995b47f0f49411aadafde2ac12ee26e5ce26
aberezn <84450664+aberezn@users.noreply.github.com>
2022-02-04 14:25:11 +0300

    Add files via upload

---
+++ workers/web-workers/worker.js
@@ -0,0 +1,27 @@
+importScripts('./node_modules/jszip/dist/jszip.min.js');
+
+self.addEventListener('message', (event) => {
+  var zip = new JSZip();
+  console.log(zip);
+  console.log('worker recieved a message: ');
+
+  console.log(event.data);
+
+  const fileName = event.data.name;
+
+  const reader = new FileReader();
+
+  reader.addEventListener('load', (event) => {
+    const content = event.target.result;
+
+    zip.file(fileName, content);
+    
+    zip.generateAsync({ type: 'blob' }).then(function(zipcontent) {
+      console.log(zipcontent);
+
+      self.postMessage(zipcontent);
+    });
+  });
+
+  reader.readAsBinaryString(event.data);
+});
